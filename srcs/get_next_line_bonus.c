64185f68195dcec62e3817d7c709e24e2b50f26c
Tuncayarda <tncyard@gmail.com>
2024-10-27 01:18:03 +0300

    bonus_part

---
+++ srcs/get_next_line_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 01:14:21 by tuaydin           #+#    #+#             */
+/*   Updated: 2024/10/27 01:16:21 by tuaydin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_del_line(char *buffer)
+{
+	char	*rtn;
+	int		i;
+
+	i = 0;
+	rtn = NULL;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\0')
+	{
+		free(buffer);
+		return (NULL);
+	}
+	rtn = ft_substr(buffer, i + 1, ft_strlen(buffer + i + 1));
+	free(buffer);
+	return (rtn);
+}
+
+char	*ft_get_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	line = ft_substr(buffer, 0, i + 1);
+	return (line);
+}
+
+char	*ft_fill_buff(char *buffer, int fd)
+{
+	char	*temp;
+	int		rd;
+
+	rd = BUFFER_SIZE;
+	while (ft_strchr(buffer, '\n') == 0 && rd == BUFFER_SIZE)
+	{
+		temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		rd = read(fd, temp, BUFFER_SIZE);
+		temp[rd] = '\0';
+		buffer = ft_strjoin(buffer, temp);
+		free(temp);
+		if (!buffer)
+			return (NULL);
+	}
+	return (buffer);
+}
+
+char	*ft_read_file(int fd)
+{
+	int		rd;
+	char	*rtn;
+
+	rtn = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!rtn)
+		return (NULL);
+	rd = read(fd, rtn, BUFFER_SIZE);
+	if (rd == -1 || rd == 0)
+	{
+		free(rtn);
+		return (NULL);
+	}
+	rtn[rd] = '\0';
+	return (rtn);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer[10000];
+	char		*line;
+
+	if (fd < 0)
+		return (NULL);
+	if (!(buffer[fd]))
+		buffer[fd] = ft_read_file(fd);
+	if (!(buffer[fd]))
+		return (NULL);
+	buffer[fd] = ft_fill_buff(buffer[fd], fd);
+	line = ft_get_line(buffer[fd]);
+	if (!line)
+		return (NULL);
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	buffer[fd] = ft_del_line(buffer[fd]);
+	return (line);
+}
---
+++ srcs/get_next_line_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 01:13:46 by tuaydin           #+#    #+#             */
+/*   Updated: 2024/10/27 01:13:58 by tuaydin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *s1);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_read_file(int fd);
+char	*ft_strrchr(char *s, int c);
+
+#endif
